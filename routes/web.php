cce07953b8e5b3ef72683a0e482a8f3f45f1d424
imfaisii <i160289@nu.edu.pk>
2022-06-19 11:59:01 +0500

    added-welcome

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return redirect()->route('login');
+    return view('welcome');
 });
 
 Route::get('/dashboard', function () {
